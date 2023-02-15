721e55d2dbcb137e0fe41538e4d69a7b032d5c45
Moroz Tetiana <tania@Morozs-MacBook-Air.local>
2023-02-15 11:45:34 +0200

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('input');
+
+inputs.forEach((input, id) => {
+  const label = document.createElement('label');
+
+  label.classList.add('field-label');
+
+  const { parentElement } = input;
+
+  parentElement.prepend(label);
+
+  label.textContent = input.name.toUpperCase();
+  input.placeholder = input.name.charAt(0).toUpperCase() + name.slice(1);
+  input.id = id;
+  label.htmlFor = id;
+});
